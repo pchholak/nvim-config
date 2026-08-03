68de94706f2db44bdc5813248f17a6c2e07b1f62
Parth Chholak <parthchholak@gmail.com>
2026-08-03 11:33:04 -0400

    removed customization to run both nvim v0.11 and v0.10

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -81,18 +81,18 @@ return {
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
+          -- -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+          -- ---@param client vim.lsp.Client
+          -- ---@param method vim.lsp.protocol.Method
+          -- ---@param bufnr? integer some lsp support methods only in specific files
+          -- ---@return boolean
+          -- local function client_supports_method(client, method, bufnr)
+          --   if vim.fn.has 'nvim-0.11' == 1 then
+          --     return client:supports_method(method, bufnr)
+          --   else
+          --     return client.supports_method(method, { bufnr = bufnr })
+          --   end
+          -- end
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
