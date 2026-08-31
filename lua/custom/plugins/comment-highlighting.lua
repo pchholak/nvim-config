1acd5ecae0b0a6e57d29e7fd5dce0778fc48b490
Parth Chholak <parthchholak@gmail.com>
2026-08-31 13:47:55 -0400

    re-add auto-install comment-highlighting for new lang

--- lua/custom/plugins/comment-highlighting.lua
+++ lua/custom/plugins/comment-highlighting.lua
@@ -30,16 +30,16 @@ return {
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'latex', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
-      -- auto_install = true,
-      -- highlight = {
-      --   enable = true,
-      --   -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --   --  If you are experiencing weird indenting issues, add the language to
-      --   --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      --   additional_vim_regex_highlighting = { 'ruby' },
-      --   disable = { 'latex' },
-      -- },
-      -- indent = { enable = true, disable = { 'ruby' } },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+        disable = { 'latex' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
     },
   },
 }
